999af1c3728ddb9bcc74c3d177d0ffdda186bcd6
Rajesh Kumar <rkumar.arya@yahoo.com>
2025-03-16 14:07:36 +0000

    Update Achievements.tsx

--- src/pages/Achievements.tsx
+++ src/pages/Achievements.tsx
@@ -74,14 +74,8 @@ const Achievements: React.FC = () => {
               </Typography>
             </CardContent>
           </Card>
- <Card className={classes.card} >
-            <CardContent>
-              <Typography variant="h6" className={classes.bold}>
-                AWS
-               </Typography>
-          <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="806d3708-1779-45ee-9e33-e922eace6153" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-            </CardContent>
-         </Card>
+ 
+         
             </div>
       ))}
       {/* <Grid item xs={12}>
@@ -103,6 +97,19 @@ const Achievements: React.FC = () => {
        
       </Grid> */}
 
+<Grid item xs={12}>
+      
+          <Card className={classes.card} >
+            <CardContent>
+               <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="806d3708-1779-45ee-9e33-e922eace6153" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+      
+            </CardContent>
+          </Card>
+       
+      </Grid>
+     <Avatar src="aws-educate-introduction-to-cloud-101.png" alt="AWS Educate Cloud 101"  variant="square"  />
+
+      
     </div>
   );
 };
